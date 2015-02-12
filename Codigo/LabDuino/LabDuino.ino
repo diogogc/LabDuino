a0e8180186f7d7d3b730a5f7dfa68a1e9c4c4aa5
Diogo Carvalho <diogo.guimaraes.carvalho@gmail.com>
2015-02-12 08:09:20 -0300

    Added Comments

--- Codigo/LabDuino/LabDuino.ino
+++ Codigo/LabDuino/LabDuino.ino
@@ -1,11 +1,11 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-
+//define digital ports wired to phone jacks (d1 to d4)
 #define d1 = 2;
 #define d2 = 3;
 #define d3 = 4;
 #define d5 = 5;
-
+//define analog ports wired to phone jacks (s1 to s4)
 #define s1 = A0;
 #define s2 = A1;
 #define s3 = A2;
@@ -15,12 +15,16 @@ LiquidCrystal_I2C lcd(0x20,16,2); // set the LCD address to 0x20 for a 16 chars 
 int screenWidth = 16;
 int screenHeight = 2;
 /*
-  ImputBotoes
+  InputBotoes
+  Buttons Input
 */
-byte x=0;
+
 /*
   Caracteres personalizados
+  Custom Character
 */
+
+// indicator 
 byte indicador[8] = {
 	0b01000,
 	0b01100,
@@ -31,7 +35,7 @@ byte indicador[8] = {
 	0b01000,
 	0b00000
 };
-
+//Home Logo
 byte homeLogo[8] = {
 	0b00000,
 	0b00000,
@@ -42,7 +46,7 @@ byte homeLogo[8] = {
 	0b10101,
 	0b10101
 };
-
+// start Logo
 byte inicioLogo[8] = {
 	0b11111,
 	0b00000,
@@ -53,7 +57,7 @@ byte inicioLogo[8] = {
 	0b00000,
 	0b11111
 };
-
+// End Logo
 byte fimLogo[8] = {
 	0b11111,
 	0b00000,
@@ -64,7 +68,7 @@ byte fimLogo[8] = {
 	0b01000,
 	0b11111
 };
-
+// Capacitor Logo
 byte capacitorLogo[8] = {
 	0b11111,
 	0b00000,
@@ -75,43 +79,47 @@ byte capacitorLogo[8] = {
 	0b00000,
 	0b11111
 };
-/*
-  Controle de Paginacao
-*/
+
+// Control flags to check when is on main option or sub options or on experiment
 boolean onSubOption = false;
 boolean isOnExperimento = false;
 
+// Stores user selected options
 int currentSelection=0;
 int optionSelected=0;
-
-String mainOption1 = "Fisica";
-String mainOption2 = "Quimica";
-String mainOption3 = "Biologia";
-String mainOption4 = "Matematica";
+// Main menu options
+String mainOption1 = "Fisica"; //Physic
+String mainOption2 = "Quimica"; // Chemestry  
+String mainOption3 = "Biologia"; // Biology
+String mainOption4 = "Matematica";// Math
 String mainOptions[4] = {mainOption1,mainOption2,mainOption3,mainOption4};
 
-String optionFisica1 = "Velocidade Med";
-String optionFisica2 = "Velocidade Inst";
-
+// Physics SubOptions
+String optionFisica1 = "Vel. Med.";
+String optionFisica2 = "Vel. Inst.";
 String subOptionFisica[2] = {optionFisica1,optionFisica2};
 
+// TODO: Add Here all other SubOtions
+
 /*
-  Pega o click do botao e muda a opcao selecionada
+  Pega o click do botao (i2c)
+  Listen for Button click (i2c)
 */
 
 int pegaInputBotao()
 {
-  Wire.requestFrom(33,2);       //Se PCF8574A - mude de 33 para 57 
+  byte x=0;
+  Wire.requestFrom(33,2);       //Se PCF8574A - mude de 33 para 57 If PCF8574A - change from 33 to 57
   if(Wire.available())     //If the request is available
   {
     x=Wire.read();       //Receive the data
   }
   if(x<255)                     //If the data is less than 255
   {
-    if (x==249) {return 1; } //Down
-    if (x==250) { return 2; } //Up
-    if (x==252) { return 3; } //Enter
-    if (x==251) { return 4; } //ES
+    if (x==241) {return 1; } //Down
+    if (x==242) { return 2; } //Up
+    if (x==244) { return 3; } //Enter
+    if (x==248) { return 4; } //ESC
   }
   else
   {
@@ -120,45 +128,48 @@ int pegaInputBotao()
 //  return x;
 }
 
+/*
+  Controle de Paginacao e muda selecao
+  Pagging control and change selected option
+*/
 void navegate(int limit)
 {
   if((currentSelection < (limit-1)))
   {
-//      if(digitalRead(2)==HIGH){
+
     if(pegaInputBotao()==2){
         currentSelection+=1;
         lcd.clear();
       }
       else
       {
-        if(currentSelection >0){
-//          if(digitalRead(3)==HIGH){
-        if(pegaInputBotao()==1){
-            currentSelection-=1;
-            lcd.clear();
-          }
+        if(currentSelection >0)
+        {
+          if(pegaInputBotao()==1){
+              currentSelection-=1;
+              lcd.clear();
+           }
         }
       }
    }
    if(currentSelection==(limit-1))
    {
-//     if(digitalRead(3)==HIGH){
-  if(pegaInputBotao()==1){
+        if(pegaInputBotao()==1){
           currentSelection-=1;
           lcd.clear();
         }
    }
    if(currentSelection==0)
    {
-//     if(digitalRead(2)==HIGH){
-  if(pegaInputBotao()==2){
+        if(pegaInputBotao()==2){
           currentSelection+=1;
           lcd.clear();
         }
    }
 }
 /*
-  Pega o click no botao "Enter"
+  Pega o click no botao "Enter". Tambem chama os experimentos.
+  Listen for enter or Esc buttons. Also Starts Experiment rotines.
 */
 void getEnterPress()
 {
@@ -170,15 +181,10 @@ void getEnterPress()
       currentSelection=0;
       lcd.clear();
     }
+    // Ir aos experimentos
+    //Go to experiments
     else
     {
-      //TODO Implementar chamar experimentos
-      if(pegaInputBotao()==4)
-      {
-        onSubOption = false;
-        currentSelection = optionSelected;
-        lcd.clear();
-      }
 //      else
 //      { 
 //        if(escapeString=="Velocidade")
@@ -191,13 +197,25 @@ void getEnterPress()
 //      }
     }
   }
+  else if(pegaInputBotao()==4)
+  {
+        if(onSubOption)
+        {
+          onSubOption = false;
+          currentSelection = optionSelected;
+          lcd.clear();
+        }
+  }
 }
 /*
-  Mostra as opcoes
+  Mostra as opcoes Main Menu
+  Shows Main Menu Options
 */
 void setSelection()
 {
   navegate(4);
+  lcd.setCursor(15,0);  //Seta a posição do cursor
+      lcd.write((uint8_t)1);
   switch (currentSelection) {
     case 0:{
       lcd.setCursor(0,0);  //Seta a posição do cursor
@@ -244,7 +262,10 @@ void setSelection()
   getEnterPress();
   delay(200);
 }
-
+/*
+  Mostra o subMenu selecionado
+  Shows selected subMenu
+*/
 void setSubSelection()
 { 
   int qtdOptions = 0;
@@ -264,83 +285,56 @@ void setSubSelection()
   qtdOptions = sizeof(mSubOpt);
   navegate(qtdOptions);
   int i;
-//  Serial.println(qtdOptions);
-//    Serial.println("<<<<< >>>>>");
-
   for(i=0; i<qtdOptions; i=i+1)
   {
     
     if(currentSelection==i)
     {
-      //Serial.println(currentSelection);
-      //Serial.println("<---------->");
       if(i%2 == 0)
       {
-//        display.setTextColor(WHITE, BLACK);
-//        display.setCursor(0,0);  //Seta a posição do cursor
-//        display.println(suboptionsA[i]);
-//        if((i+1)<=(qtdOptions -1)){
-//          display.setTextColor(BLACK,WHITE);
-//          display.setCursor(0,40);  //Seta a posição do cursor
-//          display.println(suboptionsA[i+1]);
-//        }
-//        getEnterPress(suboptionsA[i]);
-//        display.display();
-          lcd.setCursor(0,0);  //Seta a posição do cursor
+
+          lcd.setCursor(0,0);  
           lcd.write((uint8_t)0);
-          lcd.setCursor(2,0);  //Seta a posição do cursor
+          lcd.setCursor(2,0); 
           lcd.print(mSubOpt[currentSelection]);
       
-          lcd.setCursor(2,1);  //Seta a posição do cursor
+          lcd.setCursor(2,1);
           lcd.print(mSubOpt[currentSelection+1]);
         
       }
       else
       {
-//        display.setTextColor(BLACK, WHITE);
-//        display.setCursor(0,0);  //Seta a posição do cursor
-//        display.println(suboptionsA[i-1]);
-//        
-//        if((i)<=(qtdOptions -1)){
-//          display.setTextColor(WHITE,BLACK);
-//          display.setCursor(0,40);  //Seta a posição do cursor
-//          display.println(suboptionsA[i]);
-//        }
-//        getEnterPress(suboptionsA[i]);
-//        display.display();
-          lcd.setCursor(0,1);  //Seta a posição do cursor
+
+          lcd.setCursor(0,1);
           lcd.write((uint8_t)0);
-          lcd.setCursor(2,0);  //Seta a posição do cursor
+          lcd.setCursor(2,0);
           lcd.print(mSubOpt[currentSelection-1]);
       
-          lcd.setCursor(2,1);  //Seta a posição do cursor
+          lcd.setCursor(2,1);
           lcd.print(mSubOpt[currentSelection]);
       }
+      getEnterPress();
       break;
     }
   }
-  
   delay(200);
 }
 
 void setup()
 {
-  digitalWrite(2,LOW);
-  digitalWrite(3,LOW);
-  digitalWrite(7,LOW);
-  pinMode(2,INPUT);
-  pinMode(3,INPUT);
-  pinMode(7,INPUT);
-  
+ // Neste projeto Utiliza-se protocolo I2C 
   Wire.begin();
 
   lcd.init(); // initialize the lcd
+  //Cria os caracteres customizados
+  //Creates custom Characters
   lcd.createChar(0, indicador);
   lcd.createChar(1,homeLogo);
   lcd.createChar(2,inicioLogo);
   lcd.createChar(3,fimLogo);
-  lcd.begin(screenWidth,screenHeight); //lcd size
-  
+  lcd.begin(screenWidth,screenHeight);
+  //Mensagem de incializacao
+  //Startup Lettering
   lcd.setCursor(4,0);
   lcd.print("LabDuino");
   delay(2000);
@@ -348,6 +342,7 @@ void setup()
 }
 
 void loop() {
+  //Main rotine;
   if(!onSubOption){
     setSelection();
   }
@@ -355,8 +350,14 @@ void loop() {
   {
     setSubSelection();
   }
-//  lcd.scrollDisplayLeft();
-  delay(100);
+
+/* 
+   Teste de checagem dos inputs i2c
+    i2c input Check
+    
+    Para executar, comente as linhas de cima
+    To run it, comment above lines.
+*/ 
 
 // lcd.setCursor(0,0);
 //  lcd.print("X = ");
